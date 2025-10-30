fa74981d72780f45320582e0ff503f44eec39269
Ahad Baig <ahadalibaig689@gmail.com>
2025-10-29 22:57:30 -0700

    made some more changes

--- Basic files/main.py
+++ Basic files/main.py
@@ -1,10 +1,33 @@
+from read_file import read_next_arrival
+from queue import PriorityQueue
+
 event_list = []
+waiting_room = PriorityQueue()
+departure_time = 0
+departure_event = []
+patient_id_counter = 28064212
 
-while event_list:
-    current_event = event_list.pop(0)
-    print("Processing event:", current_event)
+def add_event_in_order(event):
+    index = 0
+    while index < len(event_list) and event_list[index][0] <= event[0]:
+        index += 1
+    event_list.insert(index, event)
 
-    if(current_event[1] == 'arrival'):
+with open('data1.txt', 'r') as file:
 
-
+    first_event = read_next_arrival(file, patient_id_counter)
+    if first_event:
+        event_list.append(first_event)
+        patient_id_counter += 1
     
+    while event_list:
+        current_event = event_list.pop(0)
+        print("Processing event:", current_event)
+
+        if(current_event[1] == 'arrival'):
+            waiting_room.put(current_event)
+            departure_time = current_event[0] + current_event[3] + 1
+            departure_event = (departure_time, 'departure', current_event[2], current_event[3], current_event[4])
+            
+            add_event_in_order(departure_event)
+
--- Basic files/read_file.py
+++ Basic files/read_file.py
@@ -5,27 +5,17 @@ from main import event_list
 
 #reading files 
 def read_next_arrival(file, patient_id):
-    with open('data1.txt', 'r') as file:
-        line = file.readline()
+    line = file.readline()
+    if not line:
+        # no more arrivals
+        return None  
+    #parsing the data
+    parts = line.strip().split()
+    arrival_time = int(parts[0])
+    patient_type = parts[1]
+    assessment_time = int(parts[2])
 
-        if not line:
-            None
-        #parsing the data into parts
-        parts = line.strip().split()
-        arrival_time = int(parts[0])
-        patient_type = parts[1]
-        assessment_time = int(parts[2])
-
-        #generating a patient ID starting with 28064212
-
-        patient_id = 28064212
-
-
-        #creating an arrival event
-        event = (arrival_time, 'arrival', patient_id, assessment_time, patient_type)
-
-        event_list.append(event)
-
-        return event_list
+    event = (arrival_time, 'arrival', patient_id, assessment_time, patient_type)
+    return event
 
 print(event_list)
