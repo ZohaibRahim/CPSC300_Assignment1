3ebc9113968e6553f5df62eb259beb5f071234c3
Ahad Baig <ahadalibaig689@gmail.com>
2025-10-29 20:48:08 -0700

    basic read file

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 
 #Eventlist to keep track of all the incoming files
-eventList = []
+event_list = []
 
 #reading files 
-with open('input.txt', 'r') as file:
+with open('data1.txt', 'r') as file:
     line = file.readline()
+
+
+    if not line:
+        None
+    #parsing the data into parts
+    parts = line.strip.split()
+    arrival_time = int(parts[1])
+    patient_type = parts[2]
+    assessment_time = parts[3]
+
+    #generating a patient ID starting with 28064212
+
+    patient_id = 28064212
+
+
+    #creating an arrival event
+    event = (arrival_time, 'arrival', patient_id, assessment_time, patient_type)
+
+    event_list.append(event)
+
+print(event_list)
